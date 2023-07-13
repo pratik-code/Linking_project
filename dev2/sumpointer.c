07ddf457b50896af67859cbf20be37667b563539
pratik <pratikpatil1615@gmail.com>
2023-07-13 18:53:33 +0530

    first commit

---
+++ dev2/sumpointer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int sum(int *,int);
+void main(){
+
+int a[50],n,x;
+printf("Enter a element in array");
+scanf("%d",&n);
+
+x=sum(a,n);
+printf("%d",x);
+}
+
+int sum(int* ptr,int n){
+
+int i,sum=0;
+
+for(i=0;i<n;i++){
+
+    scanf("%u",ptr+i);
+}
+for(i=0;i<n;i++){
+
+    sum=sum+*(ptr+i);
+}
+return(sum);
+}
