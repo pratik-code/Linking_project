4a4796e491fe05c87ced768e2540e43d3ed8e2c8
pratik <pratikpatil1615@gmail.com>
2023-07-13 18:56:33 +0530

    modification done in dev1

--- dev1/reversearr.c
+++ dev1/reversearr.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 void main(){
 
-int a[50],i,n;
+int a[50],i,n,z;
 
 printf("Enter a arrray limit\n");
 scanf("%d",&n);
 
+printf("Enter a elemnt in the array");
 for(i=0;i<n;i++){
 
     scanf("%d",&a[i]);
 }
-
+printf("Dispaly element in the array");
 for(i=n-1;i>=0;i--){
 
     printf("%d\t",a[i]);
